b6dec9fde6a92727ca352b8788b4c442dde5ceb5
kimhyeonkyung <khk5087@naver.com>
2020-03-25 20:53:31 +0900

    1.DetailActivity 만듬 2.bridge를 이용해 웹뷰에서 띄운 자바스크립트와 안드로이드의 자바 소스 연동

---
+++ app/src/main/java/com/sally/exhibition/DetailActivity.java
@@ -0,0 +1,20 @@
+package com.sally.exhibition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        String detailHref=getIntent().getStringExtra("detailHref");
+
+        TextView textView=findViewById(R.id.textView);
+        textView.setText(detailHref);
+    }
+}
--- app/src/main/java/com/sally/exhibition/MainActivity.java
+++ app/src/main/java/com/sally/exhibition/MainActivity.java
@@ -2,11 +2,16 @@ package com.sally.exhibition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +27,58 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
 
+        //javascriptInterface()메소드를 콜 하면서 WebAppInterface객체를 전달해준다.
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
         webView.loadUrl("http://192.168.0.26:8888/exhibition/");
+
+        /*
+
+            특정 시점에  WebView 의 javascript 실행환경에 있는 특정 함수를 호출하고 싶다면
+
+            - WebView 에 로딩된 javascript
+
+            <script>
+                function fromAndroid(arg){
+
+                }
+            </script>
+
+            - Android navive code
+             [문자열 전달]
+             String myName="kimgura";
+             webView.loadUrl("javascript:fromAndroid("+myName+");");
+
+             [object 전달]
+             String myName="kimgura";
+             webView.loadUrl("javascript:fromAndroid({num:1, name:\"kimgura\"});");
+         */
     }
 
     public class MyWebViewClient extends WebChromeClient{
 
     }
+
+    //bridge 역할을 할 innerclass
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        /*
+            서버단에서 받아온 문자열을 toast라는 변수에 담아서 받는다.
+            여러가지 데이터를 받아올 때는 JSON 문자열로 많이 받아온다.
+         */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(MainActivity.this, DetailActivity.class);
+            intent.putExtra("detailHref", toast);
+            startActivity(intent);
+        }
+    } //class WebAppInterface end
 }
